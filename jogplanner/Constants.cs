f380a881f7a430bffcbd07e31e5674eaaa4d6c3a
johnjogrady <kings07park>
2017-10-08 16:28:16 +0100

    Add project files.

---
+++ jogplanner/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace jogplanner
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://jogplanner.azurewebsites.net";
+	}
+}
+
